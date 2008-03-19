0f53cc75924a4df6ba4a6f5be850157004262396
Robert OSFIELD <robert.osfield@gmail.com>
2008-03-19 20:34:11 +0000

    Added an explict vpb:: infront of the getcwd to make sure the VPB one is used

--- src/vpb/TaskManager.cpp
+++ src/vpb/TaskManager.cpp
@@ -37,7 +37,7 @@ TaskManager::TaskManager()
     _buildName = "build";
 
     char str[2048]; 
-    _runPath = getcwd ( str, sizeof(str));
+    _runPath = vpb::getcwd ( str, sizeof(str));
     
     _defaultSignalAction = COMPLETE_RUNNING_TASKS_THEN_EXIT;
 }
