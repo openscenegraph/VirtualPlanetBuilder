50229195f0e63b96840e982ba5381c38d7bd4e39
Robert OSFIELD <robert.osfield@gmail.com>
2010-11-05 16:20:28 +0000

    Re-enabled the old fchmod usage for Win32.

--- src/vpb/PropertyFile.cpp
+++ src/vpb/PropertyFile.cpp
@@ -15,9 +15,6 @@
 #include <vpb/FileUtils>
 
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 #include <iostream>
 #include <sstream>
@@ -50,7 +47,7 @@ struct FileProxy
             vpb::fclose(file);
 
             _fileID = vpb::open (filename.c_str(), O_RDWR);
-#if 0
+#if _WIN32
             vpb::fchmod(_fileID, S_IREAD | S_IWRITE | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 #else
             vpb::fchmod(_fileID, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
